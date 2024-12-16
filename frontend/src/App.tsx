5bc8a7a843fb7b0cfee110ee01841cff31507ccf
강경원 <73323028+kkwondev@users.noreply.github.com>
2024-12-16 08:49:30 +0000

    feat: front auth

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,15 +3,15 @@ import KakaoLoginLogo from "./assets/oauth/kakao/kakao_login_medium_wide.png";
 function App() {
   const login = () => {
     window.open(
-      "http://localhost:4000/auth/kakao",
+      "http://localhost:4000/api/v1/auth/kakao",
       "kakao",
       "width=600, height=600"
     );
   };
   return (
-    <div onClick={() => login()}>
+    <a href='http://localhost:4000/auth/kakao'>
       <img src={KakaoLoginLogo} />
-    </div>
+    </a>
   );
 }
 
